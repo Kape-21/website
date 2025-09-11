285e9294782c0e4f8ea6bdb61fccbb7bffb6d89f
notwindstone <notwindstone@gmail.com>
2025-09-11 12:20:44 +0300

    feat: configure project

---
+++ unocss.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig, presetIcons, presetWind3 } from "unocss";
+
+export default defineConfig({
+  "presets": [
+
+    /*
+     * 'presetWind4' requires Chrome 111+ (March 9, 2023)
+     * Otherwise colors won't work, so not worth it
+     */
+    presetWind3({
+      // Apply dark theme only if there is a 'dark' class on parent elements
+      "dark": "class",
+    }),
+    // Lucide icons available by class names (@iconify-json/lucide)
+    presetIcons(),
+  ],
+});
