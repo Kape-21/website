f464100cb80a1299b1960d2bfc90c468b900d65e
notwindstone <notwindstone@gmail.com>
2025-09-13 00:25:14 +0300

    perf: render layout components based on window width

--- src/components/layout/Layout.vue
+++ src/components/layout/Layout.vue
@@ -2,11 +2,16 @@
 import Header from "@/components/layout/Header.vue";
 import ProgressBar from "@/components/base/ProgressBar.vue";
 import BottomBar from "@/components/layout/BottomBar.vue";
+import { useWindowSize } from "@vueuse/core";
+import { computed } from "vue";
+
+const { width } = useWindowSize();
+const isDesktop = computed(() => width.value >= 640);
 </script>
 
 <template>
   <ProgressBar />
-  <Header />
-  <BottomBar />
+  <Header v-if="isDesktop" />
+  <BottomBar v-if="isDesktop === false" />
   <slot />
 </template>
