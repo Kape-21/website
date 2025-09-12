ed30fa4ff2ef9de1a57195c166b7dc76c6d16a87
notwindstone <notwindstone@gmail.com>
2025-09-12 16:15:50 +0300

    feat: implement mobile phone swipe action

--- src/components/layout/Page.vue
+++ src/components/layout/Page.vue
@@ -1,5 +1,35 @@
+<script setup lang="ts">
+import { useSwipe } from "@vueuse/core";
+import { computed, useTemplateRef } from "vue";
+
+const element = useTemplateRef<EventTarget>("element");
+const { isSwiping, direction, lengthX } = useSwipe(element, {
+  "threshold": 50,
+});
+const swipedDistance = computed(() => Math.min(
+  Math.abs(lengthX.value) - 50,
+  256,
+));
+const isReallySwiping = computed(() => (
+  isSwiping.value && (direction.value === "left" || direction.value === "right")
+));
+</script>
+
 <template>
-  <div class="absolute pb-24 pt-4">
+  <div
+    ref="element"
+    class="absolute h-full select-none pb-24 pt-4 transition-[opacity,transform] duration-50 ease-linear"
+    :style="isReallySwiping ? {
+      transform: `translateX(${swipedDistance / 2}px)`,
+      opacity: `${Math.max(10 / swipedDistance - 0.2, 0)}`,
+    } : {}"
+  >
+    <div>
+      isReallySwiping: {{ isReallySwiping }}; direction: {{ direction }}; swipedDistance: {{ swipedDistance }}
+    </div>
     <slot />
+    <p>
+      dfj dak ;lfjklj jsdfl;ks adhfjklsadhfklj sdafkljsh adjfdk
+    </p>
   </div>
 </template>
