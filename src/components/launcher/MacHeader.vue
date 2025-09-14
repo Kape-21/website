6de58dafa7c084ecb8d28b4d3bfcb9bc8e579ab5
notwindstone <notwindstone@gmail.com>
2025-09-14 14:50:07 +0300

    feat: implement a MacHeader component

--- src/components/launcher/MacHeader.vue
+++ src/components/launcher/MacHeader.vue
@@ -2,12 +2,11 @@
 import { inject } from "vue";
 import { ApplicationName, LauncherContextKey } from "@/constants/application.ts";
 import type { ContextLauncherType } from "@/types/context-launcher.type.ts";
-import Image from "@/components/base/Image.vue";
 
 const { onlyCloseButton } = defineProps<{
   "onlyCloseButton"?: boolean;
 }>();
-const { maximized, title, maximize, minimize, close } = inject<ContextLauncherType>(LauncherContextKey) ?? {
+const { title, maximize, minimize, close } = inject<ContextLauncherType>(LauncherContextKey) ?? {
   "maximized": false,
   "title"    : ApplicationName,
   "maximize" : () => {},
@@ -19,13 +18,23 @@ const { maximized, title, maximize, minimize, close } = inject<ContextLauncherTy
 <template>
   <div class="h-6 w-full flex flex-nowrap items-center justify-between gap-2 rounded-t-md bg-catppuccin-900 sm:h-8">
     <div class="h-full flex items-stretch gap-0">
-      <button @click="close" class="group grid w-6 place-items-center sm:w-8">
-        <span class="grid h-[14px] w-[14px] place-items-center rounded-full bg-[#ff5e54] text-transparent transition-[background-color] sm:h-[18px] sm:w-[18px] group-hover-text-[#700700]">
-          <span class="sm:h-18px] h-[14px] w-[14px] sm:w-[18px] -scale-x-100" />
+      <button @click="close" class="group grid w-6 cursor-default place-items-center sm:w-8">
+        <span class="grid h-[14px] w-[14px] place-items-center rounded-full bg-[#ff5e54] text-transparent transition-[color] sm:h-[18px] sm:w-[18px] group-hover-text-[#700700]">
+          <span class="sm:h-18px] i-stash-times-solid block h-[14px] w-[14px] sm:w-[18px] -scale-x-100" />
         </span>
       </button>
-      <button @click="minimize" class="group grid w-6 place-items-center sm:w-8"></button>
-      <button @click="maximize" class="group grid w-6 place-items-center sm:w-8"></button>
+      <template v-if="!onlyCloseButton">
+        <button @click="minimize" class="group grid w-6 cursor-default place-items-center sm:w-8">
+          <span class="grid h-[14px] w-[14px] place-items-center rounded-full bg-[#ffbd2e] text-transparent transition-[color] sm:h-[18px] sm:w-[18px] group-hover:text-[#733e01]">
+            <span class="i-stash-minus-solid block h-[14px] w-[14px] sm:h-[18px] sm:w-[18px] -scale-x-100" />
+          </span>
+        </button>
+        <button @click="maximize" class="group grid w-6 cursor-default place-items-center sm:w-8">
+          <span class="grid h-[14px] w-[14px] place-items-center rounded-full bg-[#27c93f] text-transparent transition-[color] sm:h-[18px] sm:w-[18px] group-hover:text-[#01540d]">
+            <span class="i-stash-expand-diagonal-solid block h-[14px] w-[14px] sm:h-[18px] sm:w-[18px] -scale-x-100"></span>
+          </span>
+        </button>
+      </template>
     </div>
     <div class="w-fit flex flex-nowrap items-center justify-start gap-2 px-1 sm:px-2">
       <p class="text-nowrap text-xs text-white sm:text-sm">
