290f477ad3a69ec4de9d53528aae6ce656a6c017
notwindstone <notwindstone@gmail.com>
2025-09-16 01:41:13 +0300

    feat: implement a launcher status bar

--- src/components/launcher/bars/StatusBar.vue
+++ src/components/launcher/bars/StatusBar.vue
@@ -1,6 +1,110 @@
 <script setup lang="ts">
+import { computed, inject, ref, watchEffect } from "vue";
+import { useCurrentInstance } from "@/lib/stores/launcher/current-instance.ts";
+import { useIntervalFn } from "@vueuse/core";
+import { LauncherInstances, UnknownInstance } from "@/constants/launcher.ts";
+import { translate } from "@/lib/translations/translate.ts";
+import type { ContextLocaleType } from "@/types/context-locale.type.ts";
+import { LocaleContextKey } from "@/constants/application.ts";
+import { useAllInstances } from "@/lib/stores/launcher/all-instances.ts";
+import type { LauncherInstanceType } from "@/types/launcher-instance.type.ts";
+
+const locale = inject<ContextLocaleType>(LocaleContextKey);
+
+const allInstances = useAllInstances();
+const currentInstanceStore = useCurrentInstance();
+const currentInstance = computed(
+  (): LauncherInstanceType => allInstances
+    .instances
+    .find(
+      searching => searching.Id === currentInstanceStore.id,
+    ) ?? UnknownInstance,
+);
+
+const playtime = ref<Record<string, number>>(
+  Object.fromEntries(
+    LauncherInstances.map((instance): [string, number] => [instance.Id, 0]),
+  ),
+);
+
+const playtimeText = computed((): {
+  "instance": string;
+  "overall" : string;
+} => {
+  const overallSeconds = Object
+    .values(playtime.value)
+    .reduce((accumulator: number, currentValue) => currentValue + accumulator, 0);
+  const overallMinutes = Math.floor(overallSeconds / 60);
+  const overallSecondsRemaining = overallSeconds % 60;
+
+  const instanceSeconds = playtime.value[currentInstance.value.Id] ?? 0;
+  const instanceMinutes = Math.floor(instanceSeconds / 60);
+  const instanceSecondsRemaining = instanceSeconds % 60;
+
+  return {
+    "instance": currentInstance.value.Id === UnknownInstance.Id
+      ? translate("launcher.no-instance-selection", locale?.value)
+      :
+      "Minecraft" +
+      " " +
+      currentInstance.value.Version +
+      ", " +
+      translate("launcher.specific-time-played", locale?.value) +
+      " " +
+      (
+        instanceMinutes > 0
+          ? instanceMinutes + translate("launcher.minutes-short", locale?.value) + " "
+          : ""
+      ) +
+      instanceSecondsRemaining +
+      " " +
+      translate("launcher.seconds-short", locale?.value),
+    "overall":
+      translate("launcher.time-played", locale?.value) +
+      ": " +
+      (
+        overallMinutes > 0
+          ? overallMinutes + translate("launcher.minutes-short", locale?.value) + " "
+          : ""
+      ) +
+      overallSecondsRemaining +
+      " " +
+      translate("launcher.seconds-short", locale?.value),
+  };
+});
+
+const { pause, resume } = useIntervalFn(() => {
+  if (currentInstanceStore.launched === undefined) {
+    return;
+  }
+
+  playtime.value[currentInstanceStore.launched] += 1;
+}, 1000, {
+  "immediate": false,
+});
+
+watchEffect(() => {
+  if (currentInstanceStore.launched === undefined) {
+    pause();
+
+    return;
+  }
+
+  resume();
+});
 </script>
 
 <template>
-  <div></div>
+  <div class="h-9 w-full flex items-center justify-between gap-2 rounded-b-md bg-[#09090e] p-2">
+    <div class="px-4 text-nowrap text-[10px] text-[#cdd6f4] sm:text-[13px]">
+      <p class="w-fit text-nowrap">
+        {{ playtimeText.instance }}
+      </p>
+    </div>
+    <div class="px-4 text-nowrap text-[10px] text-[#cdd6f4] sm:text-[13px]">
+      <p class="w-fit text-nowrap">
+        {{ playtimeText.overall }}
+      </p>
+    </div>
+  </div>
 </template>
