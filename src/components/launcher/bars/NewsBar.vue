436cd6f0d819d7c115412d4bcdc2728335ea61ad
notwindstone <notwindstone@gmail.com>
2025-09-16 00:23:44 +0300

    feat: implement a news bar

--- src/components/launcher/bars/NewsBar.vue
+++ src/components/launcher/bars/NewsBar.vue
@@ -1,6 +1,26 @@
 <script setup lang="ts">
+import { inject } from "vue";
+import type { ContextLocaleType } from "@/types/context-locale.type.ts";
+import { LocaleContextKey } from "@/constants/application.ts";
+import { translate } from "@/lib/translations/translate.ts";
+
+const locale = inject<ContextLocaleType>(LocaleContextKey);
 </script>
 
 <template>
-  <div></div>
+  <div class="h-10 w-full flex items-stretch justify-stretch gap-2 bg-[#09090e] p-[10px]">
+    <div class="w-[5px] cursor-move rounded-full bg-mauve" />
+    <div class="w-full flex flex-nowrap items-center gap-1 rounded-md px-1 text-nowrap text-[10px] text-[#cdd6f4] transition-[background-color] hover:bg-[#1a1723] sm:text-[13px]">
+      <div class="i-fluent-news-16-regular w-4 shrink-0" />
+      <p class="w-fit text-nowrap">
+        {{ translate("launcher.website-in-vue", locale) }}
+      </p>
+    </div>
+    <div class="w-fit flex flex-nowrap items-center gap-1 rounded-md px-1 text-nowrap text-[10px] text-[#cdd6f4] transition-[background-color] hover:bg-[#1a1723] sm:text-[13px]">
+      <div class="i-fluent-news-16-regular w-4 shrink-0" />
+      <p class="min-w-20 text-nowrap">
+        {{ translate("launcher.more-news", locale) }}
+      </p>
+    </div>
+  </div>
 </template>
