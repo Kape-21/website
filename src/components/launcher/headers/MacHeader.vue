201ec53c64983c901afc05bf8f9efe32be22ce69
notwindstone <notwindstone@gmail.com>
2025-09-15 23:11:32 +0300

    fix: fix not working close button on linux & mac headers

--- src/components/launcher/headers/MacHeader.vue
+++ src/components/launcher/headers/MacHeader.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { inject } from "vue";
-import { ApplicationName, LauncherContextKey } from "@/constants/application.ts";
+import { ApplicationName } from "@/constants/application.ts";
 import type { ContextLauncherType } from "@/types/context-launcher.type.ts";
 
-const { onlyCloseButton } = defineProps<{
+const { contextKey, onlyCloseButton } = defineProps<{
+  "contextKey"      : symbol;
   "onlyCloseButton"?: boolean;
 }>();
-const { title, maximize, minimize, close } = inject<ContextLauncherType>(LauncherContextKey) ?? {
+const { title, maximize, minimize, close } = inject<ContextLauncherType>(contextKey) ?? {
   "maximized": false,
   "title"    : ApplicationName,
   "maximize" : () => {},
