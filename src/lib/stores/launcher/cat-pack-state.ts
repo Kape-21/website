a7f57459179319b6fd3b0d327947bd950fe668fa
notwindstone <notwindstone@gmail.com>
2025-09-15 11:22:40 +0300

    feat: change default cat pack state

--- src/lib/stores/launcher/cat-pack-state.ts
+++ src/lib/stores/launcher/cat-pack-state.ts
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 
 export const useCatPackState = defineStore("cat-pack", {
   "state": () => ({
-    "enabled": false,
+    "enabled": true,
   }),
   "actions": {
     "toggle"() {
