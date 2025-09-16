5920637d4d6f9e035236a50c79c22784e4860e65
windstone <notwindstone@gmail.com>
2025-09-17 02:20:16 +0300

    fix: don't show editor controls if editor is closed

--- src/lib/helpers/execute-terminal-command.ts
+++ src/lib/helpers/execute-terminal-command.ts
@@ -166,9 +166,19 @@ export function executeTerminalCommand({
       onEditor({
         "opened": true,
         "quit"  : () => {
+          onEditor({
+            "opened": false,
+            "quit"  : () => {},
+            "save"  : () => {},
+          });
           editor.emit("edit-cancel");
         },
         "save": () => {
+          onEditor({
+            "opened": false,
+            "quit"  : () => {},
+            "save"  : () => {},
+          });
           onSave(editor.value);
           editor.emit("edit-done", "");
         },
