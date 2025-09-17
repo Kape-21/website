e41d137ecfee0642fcd2d1ba209dc11e5b5dc0b9
notwindstone <notwindstone@gmail.com>
2025-09-17 23:22:47 +0300

    feat: improve details section translations

--- src/components/general/Details.vue
+++ src/components/general/Details.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import Image from "@/components/base/Image.vue";
+import { translate } from "@/lib/translations/translate.ts";
+import { inject } from "vue";
+import type { ContextLocaleType } from "@/types/context-locale.type.ts";
+import { LocaleContextKey } from "@/constants/application.ts";
+
+const locale = inject<ContextLocaleType>(LocaleContextKey);
 </script>
 
 <template>
-  <div class="mx-auto mt-12 max-w-240 flex flex-col flex-nowrap gap-4 px-8 lg:flex-row">
+  <div class="mx-auto mt-12 max-w-240 flex flex-col flex-nowrap gap-4 px-4 lg:flex-row">
     <div class="flex flex-1 flex-col items-center gap-4 rounded-lg bg-catppuccin-900 p-4">
       <Image
         class-names="w-full rounded-lg"
@@ -11,7 +17,7 @@ import Image from "@/components/base/Image.vue";
         alt="Freesm Launcher's mods section"
       />
       <p class="select-text text-center text-xl text-catppuccin-50 font-semibold">
-        Mod Management
+        {{ translate("pages.home.details.mod-management.title", locale) }}
       </p>
     </div>
     <div class="flex flex-1 flex-col gap-4">
@@ -22,7 +28,7 @@ import Image from "@/components/base/Image.vue";
           alt="Freesm Launcher's instance section"
         />
         <p class="select-text text-center text-xl text-catppuccin-50 font-semibold">
-          Modpack Importing
+          {{ translate("pages.home.details.modpack-importing.title", locale) }}
         </p>
       </div>
       <div class="flex flex-1 flex-col items-center justify-between gap-4 rounded-lg bg-catppuccin-900 p-4">
@@ -32,7 +38,7 @@ import Image from "@/components/base/Image.vue";
           alt="Freesm Launcher's accounts section"
         />
         <p class="select-text text-center text-xl text-catppuccin-50 font-semibold">
-          Auth Servers
+          {{ translate("pages.home.details.more-auth-servers.title", locale) }}
         </p>
       </div>
     </div>
