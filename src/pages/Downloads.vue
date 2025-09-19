8f0383df664fcf33c91d535f23d733e3ea92b952
notwindstone <notwindstone@gmail.com>
2025-09-20 01:49:37 +0300

    refactor: remove content from downloads page

--- src/pages/Downloads.vue
+++ src/pages/Downloads.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import Page from "@/components/layout/Page.vue";
-import Image from "@/components/base/Image.vue";
 
 document.title = "Downloads - Freesm Launcher";
 document
@@ -14,11 +13,5 @@ document
 
 <template>
   <Page>
-    <div class="pointer-events-none grid cols-2 gap-4">
-      <Image src="/photo_2025-09-04_20-16-02.jpg" alt="temp" />
-      <Image src="/photo_2025-08-29_18-35-38.jpg" alt="temp" />
-      <Image src="/bocchi-tired.jpg" alt="temp" />
-      <Image src="/assets/ame-chan.webp" alt="temp" />
-    </div>
   </Page>
 </template>
